34bb5b4629f91c88ab4c5043a5fe1fb41a8c46e0
Momcilo Krunic <momcilo.krunic@labsoft.dev>
2024-06-07 19:49:37 +0200

    #7 Fix LabSoft image logo path in Sphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_logo = "images/TransparentLogo.png"
+html_logo = "../../images/TransparentLogo.png"
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
