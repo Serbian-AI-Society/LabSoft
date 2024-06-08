fbf78b91c52c7ad66b69569f6cc5d44e8114a358
Luka Vidakovic <luka.vidakovic@labsoft.dev>
2024-06-08 07:53:41 +0200

    #6 Refactored apis.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home: React.FC<HomeProps> = ({ user, selectedChat }) => {
       // Fetch the chat history when a chat is selected
       const fetchChatHistory = async () => {
         try {
-          const response = await fetch('https://225aetnmd3.execute-api.eu-central-1.amazonaws.com/Prod/getChatHistory?ChatID=${selectedChat.chatId}', {
+          const response = await fetch(`https://225aetnmd3.execute-api.eu-central-1.amazonaws.com/Prod/getChatHistory?ChatID=${selectedChat.chatId}`, {
             method: 'GET',
           });
 
